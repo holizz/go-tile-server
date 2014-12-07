960be4451d216bd0d07cada47a78dc289ea859a4
Tom Adams <tom@holizz.com>
2014-12-06 22:39:25 -0500

    Add a benchmark

---
+++ draw_test.go
@@ -0,0 +1,40 @@
+package tiles
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"code.google.com/p/freetype-go/freetype"
+)
+
+func BenchmarkDrawTile(b *testing.B) {
+	nwPt := Point{-4.482421875, 54.162433968067795}
+	sePt := Point{-4.471435546875, 54.156001090284924}
+	scale := float64(15)
+
+	// Read font
+	font_, err := ioutil.ReadFile("example/FiraSans-Regular.ttf")
+	if err != nil {
+		b.Fatalf("Benchmark setup failed: %#v\n", err)
+	}
+
+	font, err := freetype.ParseFont(font_)
+	if err != nil {
+		b.Fatalf("Benchmark setup failed: %#v\n", err)
+	}
+
+	// Read PBF file
+	data, err := parsePbf("example/isle-of-man-latest.osm.pbf")
+	if err != nil {
+		b.Fatalf("Benchmark setup failed: %#v\n", err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := drawTile(nwPt, sePt, scale, font, data, false)
+		if err != nil {
+			b.Fatalf("Received error: %#v\n", err)
+		}
+	}
+}
